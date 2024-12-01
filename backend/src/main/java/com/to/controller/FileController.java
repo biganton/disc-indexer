bea52bb966075a7c9765e0384d81775b64e1112a
Jakub Sadkiewicz <jsadkiewicz@student.agh.edu.pl>
2024-12-01 18:08:54 +0100

    wykrywanie duplikatów, zaimplementowany Edit Distance i pare endpointów

--- backend/src/main/java/com/to/controller/FileController.java
+++ backend/src/main/java/com/to/controller/FileController.java
@@ -1,7 +1,10 @@
 package com.to.controller;
 
+import com.to.model.FileDocument;
 import com.to.service.FileService;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/files")
@@ -26,5 +29,33 @@ public class FileController {
             return "Error: " + e.getMessage();
         }
     }
+
+    @DeleteMapping("/deleteAll")
+    public String deleteAllFiles() {
+        try {
+            fileService.deleteAllFiles();
+            return "All files deleted successfully!";
+        } catch (Exception e) {
+            return "Error: " + e.getMessage();
+        }
+    }
+
+    @GetMapping("/all")
+    public List<FileDocument> getAllFiles() {
+        return fileService.getAllFiles();
+    }
+
+    @GetMapping("/duplicates")
+    public List<List<FileDocument>> getDuplicates() {
+        return fileService.findDuplicates();
+    }
+
+    @GetMapping("/versions")
+    public List<List<FileDocument>> getFileVersions(@RequestParam(defaultValue = "3") int threshold) {
+        return fileService.findFileVersions(threshold);
+    }
+
+
+
 
 }
